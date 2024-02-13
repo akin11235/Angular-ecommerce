b9083131db98a448a5ad04ec6e87a2b3481707bb
Joseph Oladunmoye <oladunmo@sheridancollege.ca>
2024-02-13 11:07:04 -0500

    complete project set up

---
+++ oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styleUrl: './bs-navbar.component.css',
+})
+export class BsNavbarComponent {}
---
+++ oshop/src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBPc58iZsLYpQkNe_GmGAdKws_WLLkWoLU',
+    authDomain: 'oshop-884db.firebaseapp.com',
+    projectId: 'oshop-884db',
+    storageBucket: 'oshop-884db.appspot.com',
+    messagingSenderId: '99613488545',
+    appId: '1:99613488545:web:77609835896ddc6b0007e0',
+    measurementId: 'G-S01ZQ1EXVB',
+  },
+};
